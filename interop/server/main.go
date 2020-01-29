9eb6d8f826638246984fdd71507e9898a86f55f1
Marten Seemann <martenseemann@gmail.com>
2020-01-29 16:04:32 +0700

    add support for the 0-RTT test case

--- interop/server/main.go
+++ interop/server/main.go
@@ -42,7 +42,7 @@ func main() {
 	tlsConf.KeyLogWriter = keyLog
 
 	switch testcase {
-	case "versionnegotiation", "handshake", "transfer", "resumption", "multiconnect":
+	case "versionnegotiation", "handshake", "transfer", "resumption", "zerortt", "multiconnect":
 		err = runHTTP09Server(quicConf)
 	case "retry":
 		// By default, quic-go performs a Retry on every incoming connection.
